b8434416737ac722d968a8cb2dc3028f22a0ba12
Ramil Vildanov <ramvildan.work@gmail.com>
2023-05-13 00:55:41 +0300

    Initial commit

---
+++ src/test/java/com/javatest/databaseTestApp/DatabaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatest.databaseTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
